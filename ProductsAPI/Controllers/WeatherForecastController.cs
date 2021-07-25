f1d51f82c9897153e93896ea3805854f9c4b1db7
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2021-07-24 23:57:40 -0300

    crud - create controller

--- ProductsAPI/Controllers/WeatherForecastController.cs
+++ ProductsAPI/Controllers/WeatherForecastController.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace ProductsAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+    //[ApiController]
+    //[Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
